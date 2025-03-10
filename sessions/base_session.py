34ea3569301a3b7de45a7e28ed983b5584defd1a
aberdasher <aberdasher.mtg@gmail.com>
2025-03-10 16:28:07 -0400

    refactor for dry and make link more visible

--- sessions/base_session.py
+++ sessions/base_session.py
@@ -99,3 +99,25 @@ class BaseSession:
                 draft_session.draft_channel_id = str(message.channel.id)
                 await session.commit()
 
+    def get_common_description(self):
+        """Generate common description parts for cube choice and draftmancer link."""
+        return (
+            f"\n\n**Chosen Cube: [{self.session_details.cube_choice}]"
+            f"(https://cubecobra.com/cube/list/{self.session_details.cube_choice})**\n\n"
+            f"**DRAFTMANCER SESSION: ➡️ [JOIN HERE]({self.session_details.draft_link})** ⬅️"
+        )
+
+    def get_thumbnail_url(self):
+        """Get thumbnail URL based on cube choice."""
+        cube_thumbnails = {
+            "LSVCube": "https://cdn.discordapp.com/attachments/1239255966818635796/1348496824956354641/LSVCube.png?ex=67cfad08&is=67ce5b88&hm=16d3350410a3a4c87404c5e6fa1c8ce0408db78a6e841a9133fd69886a1a0db8&",
+            "LSVRetro": "https://cdn.discordapp.com/attachments/1239255966818635796/1348496825417470012/LSVRetro.png?ex=67cfad09&is=67ce5b89&hm=8d4d755e1e47993910f06f886f131b2f7930a8fff022db7651ca3e976d1582ce&",
+            "PowerMack": "https://cdn.discordapp.com/attachments/1097030242507444226/1348717924978004102/mac.png?ex=67d07af3&is=67cf2973&hm=c750d1ce62a06cc0aa0b224119b4d8a04e3c35e2933cb834f819a8a11061e4f8&",
+            "AlphaFrog": "https://cdn.discordapp.com/attachments/1097030242507444226/1348723563481530378/585x620-Gavin-Thompson-Exner-2022-Profile-removebg-preview.png?ex=67d08033&is=67cf2eb3&hm=2962b1159ffafce373de1a69e527ffceec86f085453695f3348ee518e3954674&"
+        }
+        
+        # Default thumbnail for unknown cube choices
+        default_thumbnail = "https://cdn.discordapp.com/attachments/1186757246936424558/1217295353972527176/131.png"
+        
+        return cube_thumbnails.get(self.session_details.cube_choice, default_thumbnail)
+
